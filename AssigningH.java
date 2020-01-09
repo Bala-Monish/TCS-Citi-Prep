2d07304d85e961d374a6059c48ac57e1bde8b7c8
deshicabolledula <42794416+deshicabolledula@users.noreply.github.com>
2020-01-09 01:26:21 -0600

    Add files via upload

---
+++ AssigningH.java
@@ -0,0 +1,17 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class AssigningH {
+	 public static void main(String[] args) {
+		    // Create a HashMap object called capitalCities
+		    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+		    // Add keys and values (Country, City)
+		    capitalCities.put("England", "London");
+		    capitalCities.put("Germany", "Berlin");
+		    capitalCities.put("Norway", "Oslo");
+		    capitalCities.put("USA", "Washington DC");
+		    System.out.println(capitalCities);
+   }
+}
